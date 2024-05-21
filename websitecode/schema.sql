4c6a1a0dec8106b1cd414b431f977a1f051bd21c
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-05-20 21:59:08 -0400

    change(entry_id): auto_increment primary key

--- websitecode/schema.sql
+++ websitecode/schema.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE RhodySyllabi;
 
-CREATE TABLE rhodyDatabase(
-entry_id INT,
+CREATE TABLE rhodydatabase(
+entry_id INT AUTO_INCREMENT PRIMARY KEY,
 professor VARCHAR(50),
 course VARCHAR(50),
-syllabi_File VARCHAR(50),
+syllabi_File VARCHAR(500),
 syllabi_Date DATE
 );
 
-INSERT INTO rhodyDatabase(professor, course, syllabi_File, syllabi_Date)
+INSERT INTO rhodydatabase(professor, course, syllabi_File, syllabi_Date)
 values
 ('Sarah Brown', 'CSC 392', 'file', '2024-05-14');
 
