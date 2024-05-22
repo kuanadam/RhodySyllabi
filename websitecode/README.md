0fb64f6cad0765e7844e10b793a1625c8b7e7600
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-05-22 19:12:48 -0400

    chore: created new directories to organize files

--- websitecode/README.md
+++
@@ -1,1 +0,0 @@
-Files for RhodySyllabi website
--- websitecode/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('agreeCheckbox');
-    const enterButton = document.getElementById('enter')
-
-    checkbox.addEventListener('change', function() {
-        enterButton.disabled= !checkbox.checked;
-    });
-});
