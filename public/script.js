7366e56fe60131941ec5dac3504f1e1cc44bb576
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-05-23 16:08:16 -0400

    feat: confirmation popup after form submission

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// Enter button function
 document.addEventListener('DOMContentLoaded', function() {
     const checkbox = document.getElementById('agreeCheckbox');
     const enterButton = document.getElementById('enter')
@@ -5,4 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
     checkbox.addEventListener('change', function() {
         enterButton.disabled= !checkbox.checked;
     });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var model = document.getElementById("popUp");
+    var form = document.getElementById("submissionForm");
+
+    // Set a flag in session storage when the form is submitted
+    form.addEventListener('submit', function() {
+        sessionStorage.setItem('formSubmitted', 'true');
+    });
+
+    // Check the flag on page load and display the popup if the flag is set
+    if (sessionStorage.getItem('formSubmitted') === 'true') {
+        model.style.display = 'block';
+        setTimeout(function() {
+            model.style.display = 'none';
+            sessionStorage.removeItem('formSubmitted');
+        }, 3000); // 3 seconds
+    }
 });
