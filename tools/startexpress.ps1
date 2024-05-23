e65291f1521d5d307fba3c446c7d27379e6a0761
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-05-23 15:14:56 -0400

    script: quickly start express server

---
+++ tools/startexpress.ps1
@@ -0,0 +1,7 @@
+# Powershell script to quickly start Express server located in src/app.js
+
+# Set Path to Project Location
+Set-Location -Path "C:\xxxx\xxxxx\xxxx\xxxx\xxxxx\tools\" # Absolute Path to script location
+
+# Start  Express Server
+node src/app.js
