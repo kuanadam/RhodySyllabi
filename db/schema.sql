2da4d4d6afa527e3dca08832f49408b4b94dc6fb
adamk2002 <86808155+ernolfur@users.noreply.github.com>
2024-06-23 21:19:08 -0400

    refactor(db): adding contactinfo and courseinfo mysql tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,21 @@
 CREATE DATABASE RhodySyllabi;
 
-CREATE TABLE rhodydatabase(
-entry_id INT AUTO_INCREMENT PRIMARY KEY,
-professor VARCHAR(50),
-course VARCHAR(50),
-syllabi_File VARCHAR(500),
-syllabi_Date DATE,
-original_filename VARCHAR(255)
+CREATE TABLE contactinfo (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE course_Info (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    contactinfo_id INT,
+    professor VARCHAR(255) NOT NULL,
+    course_prefix VARCHAR(10) NOT NULL,
+    course_code VARCHAR(10) NOT NULL,
+    academic_season VARCHAR(50) NOT NULL,
+    academic_year INT NOT NULL,
+    syllabus_file VARCHAR(255) NOT NULL,
+    original_filename VARCHAR(255),
+    FOREIGN KEY (contactinfo_id) REFERENCES contactinfo(id) ON DELETE CASCADE
 );
